1003b5b4026f2b8de7183064a190b09786c202dc
somratsam <somratsam2@gmail.com>
2023-12-05 02:56:04 +0600

    fixed issue

--- src/app.ts
+++ src/app.ts
@@ -3,20 +3,18 @@ import cors from "cors";
 import { UserRoutes } from "./app/config/modules/user/user.route";
 const app: Application = express();
 
-
 // parsers
 
 app.use(express.json());
 app.use(cors());
 
-
-app.use('/api', UserRoutes)
+app.use("/api", UserRoutes);
 
 const getAController = (req: Request, res: Response) => {
   const a = 10;
-  res.send(a);
+  res.send(a.toString());
 };
 
-app.get('/', getAController);
+app.get("/", getAController);
 
 export default app;
